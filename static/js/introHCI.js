e61f278236c2c994cfaa0ba9399302bf441b723d
ryanmchang <ryanmchang@gmail.com>
2020-01-21 10:17:20 -0800

    First commit

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,26 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron button').text("please wait...");
+		$(".jumbotron p").addClass("active");
+		//$(".jumbotron p").toggleClass("active");
+
 	});
 
+	$(".project").click(projectClick);
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e) {
+	e.preventDefault();
+
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+      $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+			description.fadeOut();
+       //description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
 }
